dc4a59805d678e489db7b91dcb377c31560e4f1d
Salohiddin Tojiyev <129267512+thealiev@users.noreply.github.com>
2024-03-09 11:10:42 +0500

    done

---
+++ components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="footer-container">
+      <p>© {currentYear} Headphones All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  )
+}
+
+export default Footer
